610be14ea48b3d0f53b5d61a405e19806c69a346
Alex Suvorov <Suvorovalex1997@gmail.com>
2022-11-27 14:36:57 +0100

    Update lightsail-setup.sh

--- lightsail-setup.sh
+++ lightsail-setup.sh
@@ -3,32 +3,14 @@
 # To display commands
 set -x
 
-# # fix interactive install
-# export DEBIAN_FRONTEND=noninteractive
-
-# # install latest version of docker the lazy way
-# curl -sSL https://get.docker.com | sh
-
-# # make it so you don't need to sudo to run docker commands
-# usermod -aG docker ubuntu
-
-# # install docker-compose
-# curl -L https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-# chmod +x /usr/local/bin/docker-compose
-
-# mkdir -p /srv
-# chown ubuntu -R /srv
-
-# fix interactive install
+# Fix interactive install
 export DEBIAN_FRONTEND=noninteractive
 
-# Install docker
-apt update
-apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-apt install docker-ce
-usermod -aG docker ubuntu # make it so you don't need to sudo to run docker commands
+# Install latest version of docker the lazy way
+curl -sSL https://get.docker.com | sh
+
+# Make it so you don't need to sudo to run docker commands
+usermod -aG docker ubuntu
 
 # Install docker compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
