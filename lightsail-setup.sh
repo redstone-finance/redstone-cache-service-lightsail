c7de70b30734f48cae1b6329491614b158d25707
Alex Suvorov <Suvorovalex1997@gmail.com>
2022-11-27 14:25:59 +0100

    Update lightsail-setup.sh

--- lightsail-setup.sh
+++ lightsail-setup.sh
@@ -19,6 +19,14 @@ set -x
 # mkdir -p /srv
 # chown ubuntu -R /srv
 
+# Install docker
+apt update
+apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt install docker-ce
+usermod -aG docker ubuntu # make it so you don't need to sudo to run docker commands
+
 # Install docker compose
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
